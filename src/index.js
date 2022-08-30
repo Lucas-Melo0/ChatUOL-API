449810980fa18710ad0d53402c68127e8ad64665
Lucas-Melo0 <lucassss8v@gmail.com>
2022-08-30 09:18:48 -0300

    [feat] Add GET requisition to return user list

--- src/index.js
+++ src/index.js
@@ -9,17 +9,21 @@ server.post("/participants", (req, res) => {
   const user = req.body;
   const { error } = validateUser(user);
   const { name } = user;
-  const isDuplicate = users.find((value) => value.name === name);
+  const duplicates = users.find((value) => value.name === name);
   if (error) {
     return res.sendStatus(422);
   }
-  if (isDuplicate !== undefined) {
+  if (duplicates !== undefined) {
     return res.sendStatus(409);
   }
 
   users.push({ ...user, lastStatus: Date.now() });
   console.log(users);
   res.sendStatus(201);
+});
+
+server.get("/participants", (req, res) => {
+  res.send(users);
 });
 
 server.listen(5000, () => {
